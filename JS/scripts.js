ece3090baf3620150611e532afe590b36176f29c
TirkarParth <161726354+TirkarParth@users.noreply.github.com>
2024-03-21 14:00:12 +0100

    Pop up update
    Update JS file based on first task given to pop up " Hello World! " and shows food.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,10 @@
-// alert('Hello world');
+alert('Hello world');
 
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
+// let myName = 'Bob';
+// document.write(myName);
+// myName = 'John Doe';
+// document.write(myName);
+
+var favoriteFood = "Pizza"; // Create a variable named favoriteFood and assign it a value
+document.write("My favorite food is: " + favoriteFood); // Display the value stored in the favoriteFood variable on the page
+ 
