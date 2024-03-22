64ea8f8f1b573ad06f8c783189a731d1844f02cc
TirkarParth <161726354+TirkarParth@users.noreply.github.com>
2024-03-22 09:16:50 +0100

    assign an array of Pokémon objects
    Create a variable called pokemonList and assign it an array of Pokémon objects

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,5 +9,9 @@
 // document.write("My favorite food is: " + favoriteFood); // Display the value stored in the favoriteFood variable on the page
 
 
-// Create a variable called pokemonList and assign it a blank array
-var pokemonList = [];
+// Create a variable called pokemonList and assign it an array of Pokémon objects
+var pokemonList = [
+    { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
+    { name: "Charmander", height: 6, types: ['fire'] },
+    { name: "Squirtle", height: 5, types: ['water'] }
+];
