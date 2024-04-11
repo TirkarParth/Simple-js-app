31a6f99e4a7680d101f1da2c8aeefb8ce6ef5b7f
TirkarParth <161726354+TirkarParth@users.noreply.github.com>
2024-04-11 09:30:52 +0200

    var to let
    Changing var to let for better quality and better output

--- JS/scripts.js
+++ JS/scripts.js
@@ -17,13 +17,13 @@
 // }
 
 // Get the container for the Pokémon list
-var pokemonContainer = document.getElementById("pokemonList");
+let pokemonContainer = document.getElementById("pokemonList");
 
 // Create a new pokemonRepository variable to hold the IIFE return value
-var pokemonRepository = (function () {
+let pokemonRepository = (function () {
 
     // Create a variable called pokemonList and assign it an array of Pokémon objects
-    var pokemonList = [
+    let pokemonList = [
         { name: "Bulbasaur", height: 7, types: ['grass', 'poison'] },
         { name: "Charmander", height: 6, types: ['fire'] },
         { name: "Squirtle", height: 5, types: ['water'] }
@@ -38,14 +38,14 @@ var pokemonRepository = (function () {
 
     // Define the addListItem function to create buttons for each Pokémon
     function addListItem(pokemon) {
-        var button = document.createElement('button');
+        let button = document.createElement('button');
         button.innerText = pokemon.name;
         button.classList.add('pokemon-button');
 
         // Call the function to add event listener
         addButtonEventListener(button, pokemon);
 
-        var listItem = document.createElement('li');
+        let listItem = document.createElement('li');
         listItem.appendChild(button);
         pokemonContainer.appendChild(listItem);
     }
@@ -78,19 +78,19 @@ var pokemonRepository = (function () {
 // Iterate over each Pokémon in the pokemonRepository array
 pokemonRepository.getAll().forEach(function (pokemon) {
     // Create a div element to represent the Pokémon card
-    var pokemonCard = document.createElement("div");
+    let pokemonCard = document.createElement("div");
     pokemonCard.classList.add("pokemon-card");
 
     // Create elements to display Pokémon name, height, and types
-    var nameElement = document.createElement("div");
+    let nameElement = document.createElement("div");
     nameElement.classList.add("pokemon-name");
     nameElement.textContent = pokemon.name;
 
-    var heightElement = document.createElement("div");
+    let heightElement = document.createElement("div");
     heightElement.classList.add("pokemon-height");
     heightElement.textContent = "Height: " + pokemon.height;
 
-    var typesElement = document.createElement("div");
+    let typesElement = document.createElement("div");
     typesElement.classList.add("pokemon-types");
     typesElement.textContent = "Types: " + pokemon.types.join(", ");
 
